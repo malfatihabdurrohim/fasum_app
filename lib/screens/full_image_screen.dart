c34e666d2295e901e124fb6d786e56d6ca523ea7
malfatihabdurrohim <malfatihabdurrohim@gmail.com>
2026-05-20 14:38:41 +0700

    Update full_image_screen.dart

--- lib/screens/full_image_screen.dart
+++ lib/screens/full_image_screen.dart
@@ -2,25 +2,29 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 
 class FullImageScreen extends StatefulWidget {
+  const FullImageScreen({
+    super.key,
+    required this.imageBase64
+    });
   final String imageBase64;
-  const FullImageScreen({super.key, required this.imageBase64});
-
   @override
-  State<FullImageScreen> createState() => _FullImageScreenState();
+  State<FullImageScreen> createState() => FullImageScreenState();
 }
 
-class _FullImageScreenState extends State<FullImageScreen> {
+class FullImageScreenState extends State<FullImageScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
+    return Scaffold(
       backgroundColor: Colors.black,
       body: GestureDetector(
         onTap: () {
           Navigator.pop(context);
         },
         child: Center(
-          child: InteractiveViewer(child: Image.memory(base64Decode(imageBase64),
-          fit: BoxFit.contain,
+          child: InteractiveViewer(
+            child: Image.memory(
+              base64Decode(widget.imageBase64),
+              fit: BoxFit.contain,
             ),
           ),
         ),
